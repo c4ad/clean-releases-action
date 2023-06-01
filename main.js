8614740b1904789e99dc5c1ef6581f9d49ffbe74
Oliver Frank <oliverfrank321@gmail.com>
2023-06-01 14:30:31 +0100

    Initial commit

---
+++ main.js
@@ -0,0 +1,19 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+(async () => {
+  const GITHUB_TOKEN = core.getInput("token");
+  const octokit = github.getOctokit(GITHUB_TOKEN);
+
+  const owner = github.context.payload.repository.owner.login;
+  const repo = github.context.payload.repository.name;
+
+
+  const { data: pullRequest } = await octokit.rest.pulls.get({
+    owner,
+    repo,
+    pull_number: 1,
+  });
+
+  console.log(pullRequest);
+})();
