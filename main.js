7a5a562bd45252de029fe304776cf7c39cd6ceb2
Oliver Frank <oliverfrank321@gmail.com>
2023-06-01 15:50:25 +0100

    dev

--- main.js
+++ main.js
@@ -26,7 +26,9 @@ const github = require('@actions/github');
   oldestDate.setMonth(oldestDate.getMonth() - maxAge);
   const releasesToDelete = releases.filter((release) => {
     const releaseDate = new Date(Date.parse(release.created_at));
-    releaseDate.setMonth(releaseDate.getMonth() - 12);
+    if (release.name === "v1.0") {
+      releaseDate.setMonth(releaseDate.getMonth() - 12);
+    }
 
     return releaseDate < oldestDate;
   }).map((release) => {
@@ -60,9 +62,11 @@ const github = require('@actions/github');
       tag_name: tag.tag,
       commit_date: commit.author.date,
     };
-  }))).filter(({commit_date}) => {
+  }))).filter(({tag_name, commit_date}) => {
     const tagDate = new Date(Date.parse(commit_date));
-    tagDate.setMonth(tagDate.getMonth() - 12);
+    if (tag_name === "v1.1") {
+      tagDate.setMonth(tagDate.getMonth() - 12);
+    }
 
     return tagDate < oldestDate;
   }).map((({tag_name}) => ({tag_name})));
